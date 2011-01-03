253a48c35408d4e41e04a6775aae34564509cf1e
George Lee <keoki.lee@gmail.com>
2011-01-02 15:50:52 -1000

    Created recursive function for counting words in a sentence.

---
+++ Erlang/day1/num_words.erl
@@ -0,0 +1,16 @@
+-module(num_words).
+-export([num_words/1]).
+
+% Recursive function for counting words.  Not exported by the module.
+% First argument is the sentence.  Second argument is true if we are in a word, false otherwise.
+num_words_recursive([], false) -> 0;
+% 32 corresponds to a space.
+num_words_recursive([32|Tail], false) -> num_words_recursive(Tail, false);
+num_words_recursive([_|Tail], false) -> num_words_recursive(Tail, true);
+
+num_words_recursive([], true) -> 1;
+num_words_recursive([32|Tail], true) -> 1 + num_words_recursive(Tail, false);
+num_words_recursive([_|Tail], true) -> num_words_recursive(Tail, true).
+
+num_words([]) -> 0;
+num_words(Words) -> num_words_recursive(Words, false).
